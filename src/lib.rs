4cd7492902e775ff0c40268fc150addc7ef3065c
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-08-20 13:10:33 +0900

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 pub fn entry() {
     println!("Hello, world!");
 }
+
+pub fn render(ss: String) -> String {
+    unimplemented!();
+}
--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,32 @@
 extern crate etxt;
 
+use std::fs;
+use std::io::{Read};
 use etxt::*;
 
 #[test]
 fn test_1() {
     entry();
+}
+
+fn read_file(filename: &str) -> String {
+    let mut file = fs::File::open(filename).unwrap();
+    let mut ss = String::new();
+    file.read_to_string(&mut ss).unwrap();
+    ss
+}
+
+#[test]
+fn test_read_file() {
+    let ss = read_file("tests/testcases/in001.etxt");
+    eprintln!("{}", ss);
+    assert_eq!(ss, "\"abcd\\n\";\n");
+}
+
+#[test]
+fn test_2() {
+    let input = read_file("tests/testcases/in001.etxt");
+    let exp_output = read_file("tests/testcases/out001.txt");
+    let output = etxt::render(input);
+    assert_eq!(exp_output, output);
 }
