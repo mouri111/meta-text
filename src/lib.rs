3efc78c84e92015b90605884e5e7c0e649119156
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-08-20 14:33:35 +0900

    Add Token

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@ pub fn entry() {
     println!("Hello, world!");
 }
 
+#[derive(Debug,PartialEq,Eq,Clone)]
+pub enum Token {
+    IDENT(Vec<char>),
+    NUM(Vec<char>),
+    OP(Vec<char>),
+    DELIMITER(Vec<char>),
+    STRING(Vec<char>),
+}
 pub fn render(ss: String) -> String {
     unimplemented!();
 }
