b7d3d6d91f493db082028a843a9bc738bdcbb081
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-08-27 17:52:56 +0900

    Add Precedence

--- src/lib.rs
+++ src/lib.rs
@@ -118,6 +118,55 @@ fn lex(ss: &Vec<char>) -> Vec<Token> {
     res
 }
 
+#[derive(Debug,PartialEq,Eq,Clone,Copy)]
+pub enum Precedence {
+    LeftAssociative(i32),
+    RightAssociative(i32),
+    NonAssociative,
+}
+
+impl Precedence {
+    fn cont(&self, prec: &Precedence) -> bool {
+        match self {
+            Precedence::LeftAssociative(p1) => {
+                match prec {
+                    Precedence::LeftAssociative(p2) => {
+                        p1 < p2
+                    }
+                    Precedence::RightAssociative(p2) => {
+                        if p1 == p2 {
+                            panic!();
+                        }
+                        p1 < p2
+                    }
+                    Precedence::NonAssociative => {
+                        panic!()
+                    }
+                }
+            }
+            Precedence::RightAssociative(p1) => {
+                match prec {
+                    Precedence::LeftAssociative(p2) => {
+                        if p1 == p2 {
+                            panic!();
+                        }
+                        p1 <= p2
+                    }
+                    Precedence::RightAssociative(p2) => {
+                        p1 <= p2
+                    }
+                    Precedence::NonAssociative => {
+                        panic!();
+                    }
+                }
+            }
+            Precedence::NonAssociative => {
+                false
+            }
+        }
+    }
+}
+
 pub fn parse(ts: Vec<Token>) -> AST {
     let mut xs = vec![];
     let mut iter = ts.iter();
