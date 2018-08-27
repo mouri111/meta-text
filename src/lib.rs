2398a7d863336b27cf8722d8e2a8aaa3588add4f
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-08-27 17:57:45 +0900

    Add "parse_atom" and "parse_expression_sub"

--- src/lib.rs
+++ src/lib.rs
@@ -179,6 +179,88 @@ use std::collections::BTreeMap;
 use std::slice::Iter;
 use std::iter::Peekable;
 
+pub fn parse_atom(iter: &mut Peekable<Iter<Token>>, precedence_table: &BTreeMap<Vec<char>, Precedence>) -> Expression {
+    if let Some(head) = iter.peek() {
+        match head {
+            Token::NUM(_) => {
+            }
+            Token::DELIMITER(_) => {
+            }
+            _ => {
+                panic!();
+            }
+        }
+    }
+    else {
+        panic!();
+    }
+    if let Some(head) = iter.next() {
+        match head {
+            Token::NUM(_) => {
+                Expression::Literal(head.clone())
+            }
+            Token::DELIMITER(d) => {
+                if *d == vec!['('] {
+                    let res = parse_expression(iter, precedence_table);
+                    iter.next();
+                    res
+                }
+                else {
+                    panic!();
+                }
+            }
+            _ => {
+                panic!();
+            }
+        }
+    }
+    else {
+        panic!();
+    }
+}
+
+pub fn parse_expression_sub(iter: &mut Peekable<Iter<Token>>, precedence_table: &BTreeMap<Vec<char>,Precedence>, prev_prec: Option<Precedence>) -> Expression {
+    let mut res = parse_atom(iter, precedence_table);
+    loop {
+        if let Some(head) = iter.peek() {
+            match head {
+                Token::OP(ss) => {
+                    eprintln!("ss = {:?}", ss);
+                    let prec = precedence_table[ss];
+                    if prev_prec.is_none() || prev_prec.unwrap().cont(&prec) {
+                    }
+                    else {
+                        break;
+                    }
+                }
+                Token::DELIMITER(_) => {
+                    break;
+                }
+                _ => {
+                    eprintln!("{:?}", head);
+                    panic!();
+                }
+            }
+        }
+        else {
+            break;
+        }
+        if let Some(head) = iter.next() {
+            match head {
+                Token::OP(ss) => {
+                    let prec = precedence_table[ss];
+                    let right = parse_expression_sub(iter, precedence_table, Some(prec));
+                    res = Expression::BinaryOp(Box::new(res), head.clone(), Box::new(right));
+                }
+                _ => {
+                    panic!();
+                }
+            }
+        }
+    }
+    res
+}
+
 pub fn parse(ts: Vec<Token>) -> AST {
     let mut xs = vec![];
     let mut iter = ts.iter();
