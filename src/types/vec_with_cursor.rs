16c55f855e94e3826f0df659221472cddc34ef51
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-09-10 22:20:32 +0900

    Add a test of VecWithCursor::next

--- src/types/vec_with_cursor.rs
+++ src/types/vec_with_cursor.rs
@@ -11,4 +11,20 @@ impl<'a, T> VecWithCursor<'a, T> {
             p: 0
         }
     }
+    pub fn next(&self) -> Option<(&'a T, VecWithCursor<'a, T>)> {
+        unimplemented!();
+    }
+}
+
+#[test]
+fn test_next() {
+    let xs = vec![1, 2, 3];
+    let xs = VecWithCursor::new(&xs);
+    let (x, xs) = xs.next().unwrap();
+    assert_eq!(1, *x);
+    let (x, xs) = xs.next().unwrap();
+    assert_eq!(2, *x);
+    let (x, xs) = xs.next().unwrap();
+    assert_eq!(3, *x);
+    assert!(xs.next().is_none());
 }
