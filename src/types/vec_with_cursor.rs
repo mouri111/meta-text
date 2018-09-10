0ace129947cb07ec953f657465734c87e72c0d48
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2018-09-10 22:01:59 +0900

    Add VecWithCursor::new

--- src/types/vec_with_cursor.rs
+++ src/types/vec_with_cursor.rs
@@ -3,3 +3,12 @@ pub struct VecWithCursor<'a, T: 'a> {
     xs: &'a Vec<T>,
     p: usize,
 }
+
+impl<'a, T> VecWithCursor<'a, T> {
+    pub fn new(xs: &'a Vec<T>) -> VecWithCursor<'a, T> {
+        VecWithCursor {
+            xs,
+            p: 0
+        }
+    }
+}
